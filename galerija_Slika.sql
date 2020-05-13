e8b56d50ae56a5fdd0270a0fd4c0092f4b8244fa
Hrvoje Škrbina <574515c0d1ng@gmail.com>
2020-05-13 20:56:13 +0200

    Galerija slika - dodani muzeji, direktor, djelo, autor, kustosi

--- galerija_Slika.sql
+++ galerija_Slika.sql
@@ -6,7 +6,8 @@ CREATE TABLE djelo(
 	sifra int NOT NULL primary key auto_increment,
     naziv varchar(50) NOT NULL,
     opis text,
-    autor int NOT NULL
+    autor int NOT null,
+    muzej int not NULL
 );
 
 CREATE TABLE osoba(
@@ -19,29 +20,54 @@ CREATE TABLE osoba(
 
 CREATE TABLE autor(
 	sifra int NOT NULL PRIMARY KEY auto_increment,
-	osoba int	
+	osoba int not NULL
 );
 
 CREATE TABLE kustos(
 	sifra int NOT NULL PRIMARY KEY auto_increment,
-	osoba int
+	osoba int not NULL
+);
+
+CREATE TABLE direktor(
+	sifra int NOT NULL PRIMARY KEY auto_increment,
+	osoba int not NULL
+);
+
+create table muzej(
+	sifra int NOT NULL PRIMARY KEY auto_increment,
+	naziv varchar(50) not null,
+	lokacija varchar(150) not null,
+	osnovan datetime,
+	tip varchar(50),
+	direktor int not NULL
 );
 
 alter table djelo add foreign key (autor) references osoba(sifra);
 alter table autor add foreign key (osoba) references osoba(sifra);
 alter table kustos add foreign key (osoba) references osoba(sifra);
+alter table direktor add foreign key (osoba) references osoba(sifra);
+alter table muzej add foreign key (direktor)  references direktor(sifra);
 
 insert into osoba (ime, prezime, mjesto_Rodenja, Datum_Rodenja) values
+('Glenn David', 'Lowry', 'New York City, New York', '1954-09-28'),
 ('Vincent', 'van Gogh', 'Zundert', '1853-03-30'),
-('Thelma', 'Golden', 'St. Albans, Queens', '1965-09-22');
+('Ann', 'Temkin', 'Torrington, Connecticut', '1959-12-26'),
+('Thaddeus John', 'Szarkowski', 'Ashland, Wisconsin', '1925-12-18');
 
-insert into autor (osoba) values (1);
-insert into kustos (osoba) values (2);
+
+insert into direktor (osoba) values (1);
+insert into autor (osoba) values (2);
+insert into kustos (osoba) values (3), (4);
+
+# describe muzej;
+
+insert into muzej (naziv, lokacija, osnovan, tip, direktor) values
+('Museum of Modern Art', '11 West 53rd Street, Manhattan, New York City', '1929-11-07', 'Muzej umjetnosti', 1);
 
 # describe djelo;
 
-insert into djelo  (naziv, opis, autor) values
-('Zvjezdana noć', 'Zvjezdana noć je Van Goghovo ulje na platnu, slavno remek-djelo postimpresionizma, iz 1889. godine.', 1);
+insert into djelo  (naziv, opis, autor, muzej) values
+('Zvjezdana noć', 'Zvjezdana noć je Van Goghovo ulje na platnu, slavno remek-djelo postimpresionizma, iz 1889. godine.', 2, 1);
 
 # select * from djelo;
 # select * from autor;
